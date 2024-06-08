5f9033b21f3bc6c6903d92640716ca373ea987d2
Shailendra Singh <73794877+shailendrasingh-cyber@users.noreply.github.com>
2024-06-08 16:32:45 +0530

    Add files via upload
    add new feature

---
+++ app3.py
@@ -0,0 +1,54 @@
+import cv2
+import pyaudio
+import wave
+
+def play_sound(file):
+    chunk = 1024
+    wf = wave.open(file, 'rb')
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+    data = wf.readframes(chunk)
+
+    while data:
+        stream.write(data)
+        data = wf.readframes(chunk)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam.
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        # Play the sound
+        play_sound('alert.wav')
+
+    # Display
+    cv2.imshow('img', img)
+
+    # Stop if escape key is pressed
+    if cv2.waitKey(30) & 0xFF == 27:
+        break
+
+# Release the VideoCapture object
+cap.release()
+cv2.destroyAllWindows()
